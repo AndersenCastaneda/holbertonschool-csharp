60ae0c7014ccbbb6bb7c447945a757143383a635
AndersenCastaneda <andersenpiano@gmail.com>
2020-12-01 15:43:21 -0500

    Add tasks 5 - 7

---
+++ 0x01-csharp-ifelse_loops_methods/5-print_comb/5-print_comb.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        for (int i = 0; i <= 99; i++)
+            Console.Write((i != 99) ? $"{i:D2}, " : $"{i}\n");
+    }
+}
---
+++ 0x01-csharp-ifelse_loops_methods/7-islower/7-islower.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+class Character
+{
+    public static bool IsLower(char c)
+    {
+        bool r = false;
+        for (char i = 'a'; i <= 'z'; i++)
+        {
+            if (c == i)
+                r = true;
+        }
+
+        return (r);
+    }
+}
