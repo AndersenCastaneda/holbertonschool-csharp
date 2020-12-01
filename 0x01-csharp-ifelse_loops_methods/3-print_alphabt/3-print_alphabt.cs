abfcadd503a90f1735457bb03801e29f03b280f5
AndersenCastaneda <andersenpiano@gmail.com>
2020-12-01 14:59:18 -0500

    dotnet 2.1

---
+++ 0x01-csharp-ifelse_loops_methods/3-print_alphabt/3-print_alphabt.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        char letter = 'a';
+
+        while (letter <= 'z')
+        {
+            if (letter != 'q' && letter != 'e')
+                Console.Write($"{letter}");
+
+            letter++;
+        }
+    }
+}
