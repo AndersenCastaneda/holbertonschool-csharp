7bdea421a8a2b100087ea78ec4288fc6aca29f9c
AndersenCastaneda <andersenpiano@gmail.com>
2020-12-01 16:12:48 -0500

    Add tasks 8 - 9

---
+++ 0x01-csharp-ifelse_loops_methods/8-print_last_digit/8-print_last_digit.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+class Number
+{
+    public static int PrintLastDigit(int number)
+    {
+        int lastDigit;
+
+        if (number < 0)
+            lastDigit = (number % 10) * -1;
+        else
+            lastDigit = number % 10;
+
+        Console.Write($"{lastDigit}");
+        return lastDigit;
+    }
+}
