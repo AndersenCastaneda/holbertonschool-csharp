636c72efbf645d376f9df1bac3d4f27120427f5b
AndersenCastaneda <andersenpiano@gmail.com>
2020-12-02 13:11:27 -0500

    ADd tasks 5 - 7

---
+++ 0x02-csharp-arrays_lists_dictionaries/5-max_integer/5-max_integer.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+class List
+{
+    public static int MaxInteger(List<int> myList)
+    {
+        int bigger = Int32.MinValue, length;
+
+        if (myList == null || myList.Count == 0)
+        {
+            Console.WriteLine("List is empty");
+            return -1;
+        }
+
+        length = myList.Count;
+        for (int i = 0; i < length; i++)
+        {
+            if (myList[i] > bigger)
+                bigger = myList[i];
+        }
+
+        return bigger;
+    }
+}
---
+++ 0x02-csharp-arrays_lists_dictionaries/6-divisible_by_2/6-divisible_by_2.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+class List
+{
+    public static List<bool> DivisibleBy2(List<int> myList)
+    {
+        List<bool> boolList = new List<bool>();
+
+        int length = myList.Count;
+        for (int i = 0; i < length; i++)
+            boolList.Add((myList[i] % 2 == 0));
+
+        return boolList;
+    }
+}
