48ef6656cdd46f67ef0433311035b56b65dedf14
AndersenCastaneda <andersenpiano@gmail.com>
2020-12-02 14:47:28 -0500

    Add task 13

---
+++ 0x02-csharp-arrays_lists_dictionaries/13-best_score/13-best_score.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Dictionary
+{
+    public static string BestScore(Dictionary<string, int> myList)
+    {
+        int score = 0;
+        string bestScore = "";
+        bool isSet = false;
+
+        if (myList == null || myList.Count == 0)
+            return "None";
+
+        foreach (var item in myList)
+        {
+            if (!isSet)
+            {
+                score = item.Value;
+                bestScore = item.Key;
+                isSet = true;
+            }
+
+            if (item.Value > score)
+            {
+                score = item.Value;
+                bestScore = item.Key;
+            }
+        }
+
+        return bestScore;
+    }
+}
