e6ffe2259fe02f61fb9300d6407434734d263e76
AndersenCastaneda <andersenpiano@gmail.com>
2020-12-02 14:55:47 -0500

    Add task 14

---
+++ 0x02-csharp-arrays_lists_dictionaries/14-rectangular_array/14-rectangular_array.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int length  = 5;
+        int[,] twoDimArray = new int[length, length];
+        twoDimArray[2, 2] = 1;
+
+        for (int i = 0; i < length; i++)
+        {
+            for (int j = 0; j < length; j++)
+            {
+                Console.Write((j != length - 1) ?
+                    $"{twoDimArray[i, j]} " : $"{twoDimArray[i, j]}\n");
+            }
+        }
+    }
+}
