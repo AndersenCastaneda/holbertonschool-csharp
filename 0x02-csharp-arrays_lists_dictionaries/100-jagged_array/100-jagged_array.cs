83bb9d4d23b1e4caf5ed4c2b8d0d81c2722a250c
AndersenCastaneda <andersenpiano@gmail.com>
2020-12-02 15:25:11 -0500

    Add task 16

---
+++ 0x02-csharp-arrays_lists_dictionaries/100-jagged_array/100-jagged_array.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int height = 3;
+        int width;
+        int[][] jaggedArray = new int[3][] {
+            new int[] { 0, 1, 2, 3 },
+            new int[] { 0, 1, 2, 3, 4, 5, 6 },
+            new int[] {0, 1 },
+        };
+
+
+        for (int i = 0; i < height; i++)
+        {
+            width = jaggedArray[i].Length;
+            for (int j = 0; j < width; j++)
+                Console.Write((j != width - 1) ? $"{j} " : $"{j}\n");
+        }
+    }
+}
