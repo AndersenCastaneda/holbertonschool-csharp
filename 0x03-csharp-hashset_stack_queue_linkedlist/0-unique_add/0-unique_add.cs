9768a239917154b0bdaab35e86c4ff2e6cd09af9
AndersenCastaneda <andersenpiano@gmail.com>
2020-12-03 12:17:38 -0500

    Add task 0

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/0-unique_add/0-unique_add.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class List
+{
+    public static int Sum(List<int> myList)
+    {
+        int sum = 0;
+        if (myList == null || myList.Count == 0)
+            return 0;
+
+        List<int> newList = myList.Distinct().ToList();
+        int length = newList.Count;
+        for (int i = 0; i < length; i++)
+            sum += newList[i];
+
+        return sum;
+    }
+}
