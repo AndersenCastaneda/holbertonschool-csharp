7da6528b8402476f673e95241eb796472845c507
AndersenCastaneda <andersenpiano@gmail.com>
2020-12-03 12:56:28 -0500

    Add task 1

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/1-common_elements/1-common_elements.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+class List
+{
+    public static List<int> CommonElements(List<int> list1, List<int> list2)
+    {
+        int length1 = list1.Count;
+        int length2 = list2.Count;
+        List<int> newList = new List<int>();
+
+        if (list1 == null || length1 == 0 || list2 == null || length2 == 0)
+            return newList;
+
+        for (int i = 0; i < length1; i++)
+        {
+            for (int j = 0; j < length2; j++)
+            {
+                if (list1[i] == list2[j])
+                    newList.Add(list1[i]);
+            }
+        }
+
+        return newList;
+    }
+}
