d54efa6954d776746cea404e2cd77d7762a95ab2
AndersenCastaneda <andersenpiano@gmail.com>
2020-12-03 16:38:25 -0500

    Add task 8

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/8-linkedlist_find/8-linkedlist_find.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+class LList
+{
+    public static int FindNode(LinkedList<int> myLList, int value)
+    {
+        if (myLList == null)
+            return -1;
+
+        int index = 0;
+        foreach (var item in myLList)
+        {
+            if (item == value)
+                return index;
+            index++;
+        }
+
+        return -1;
+    }
+}
