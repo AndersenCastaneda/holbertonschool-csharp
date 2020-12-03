aa0a98e690dc169344f4194ef46d75e39e7bcd30
AndersenCastaneda <andersenpiano@gmail.com>
2020-12-03 16:03:43 -0500

    Add task 7

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/7-linkedlist_add/7-linkedlist_add.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+
+class LList
+{
+    public static LinkedListNode<int> Add(LinkedList<int> myLList, int n)
+    {
+        if (myLList == null)
+            myLList = new LinkedList<int>();
+
+        LinkedListNode<int> myLListNode = myLList.AddFirst(n);
+        return myLListNode;
+    }
+}
