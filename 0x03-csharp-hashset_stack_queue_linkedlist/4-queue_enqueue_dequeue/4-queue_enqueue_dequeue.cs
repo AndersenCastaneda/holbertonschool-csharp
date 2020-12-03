3f94262d210c65024e3b1f725fa7ecab10c23abc
AndersenCastaneda <andersenpiano@gmail.com>
2020-12-03 14:29:49 -0500

    Add task 4

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/4-queue_enqueue_dequeue/4-queue_enqueue_dequeue.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+class MyQueue
+{
+    public static Queue<string> Info(Queue<string> aQueue, string newItem, string search)
+    {
+        if (aQueue == null)
+            aQueue = new Queue<string>();
+
+        Console.WriteLine($"Number of items: {aQueue.Count}");
+        Console.WriteLine((aQueue.Count == 0) ? "Queue is empty" : $"First item: {aQueue.Peek()}");
+
+        if (newItem != null)
+            aQueue.Enqueue(newItem);
+
+        Console.WriteLine($"Stack contains \"{search}\": {aQueue.Contains(search)}");
+        while (aQueue.Contains(search))
+            aQueue.Dequeue();
+
+        return aQueue;
+    }
+}
