6d524192a0ebd5b02560785663d9a66f00e42118
AndersenCastaneda <andersenpiano@gmail.com>
2020-12-03 17:17:58 -0500

    Add task 10

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/10-linkedlist_get_node/10-linkedlist_get_node.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+class LList
+{
+    public static int GetNode(LinkedList<int> myLList, int n)
+    {
+        int length = myLList.Count;
+
+        if (myLList == null || n >= length)
+            return 0;
+
+        LinkedListNode<int> node = myLList.First;
+        for (int i = 0; i < n; i++)
+            node = node.Next;
+
+        return node.Value;
+    }
+}
