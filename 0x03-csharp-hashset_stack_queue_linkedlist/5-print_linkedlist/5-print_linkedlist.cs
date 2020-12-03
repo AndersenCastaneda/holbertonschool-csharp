e778839d28e26712ba6b9b7783de6c89e138e9f6
AndersenCastaneda <andersenpiano@gmail.com>
2020-12-03 14:59:04 -0500

    Add task 5

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/5-print_linkedlist/5-print_linkedlist.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+class LList
+{
+    public static LinkedList<int> CreatePrint(int size)
+    {
+        int length = (size < 0) ? 0 : size;
+        LinkedList<int> myList = new LinkedList<int>();
+
+        for (int i = 0; i < length; i++)
+        {
+            Console.WriteLine($"{i}");
+            myList.AddLast(i);
+        }
+
+        return myList;
+    }
+}
