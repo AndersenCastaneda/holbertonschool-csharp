b40d1f10a38f985b175e1fb0862669252e1bc903
AndersenCastaneda <andersenpiano@gmail.com>
2020-12-15 12:50:16 -0500

    Add mandatory tasks

---
+++ 0x06-csharp-classes/4-enemy/4-enemy.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Enemies
+{
+    public class Zombie
+    {
+        private int health;
+        private string name = "(No name)";
+
+        public Zombie()
+        {
+            this.health = 0;
+        }
+
+        public Zombie(int value)
+        {
+            if (value < 0)
+                throw new ArgumentException("Health must be greater than or equal to 0");
+
+            this.health = value;
+        }
+
+        public int GetHealth() => health;
+
+        public string Name
+        {
+            get
+            {
+                return name;
+            }
+            set
+            {
+                name = value;
+            }
+        }
+    }
+}
