074b01078e271661f835b35d78bbb7e3b447eca5
AndersenCastaneda <andersenpiano@gmail.com>
2020-12-15 13:10:56 -0500

    Add documentation

--- 0x06-csharp-classes/1-enemy/1-enemy.cs
+++ 0x06-csharp-classes/1-enemy/1-enemy.cs
@@ -1,11 +1,18 @@
-﻿using System;
-
-namespace Enemies
+﻿namespace Enemies
 {
+    /// <summary>
+    /// Class Zombie
+    /// </summary>
     public class Zombie
     {
+        /// <summary>
+        /// Zombie health
+        /// </summary>
         public int health;
 
+        /// <summary>
+        /// Creates a new Zombie and sets health to 0
+        /// </summary>
         public Zombie()
         {
             this.health = 0;
