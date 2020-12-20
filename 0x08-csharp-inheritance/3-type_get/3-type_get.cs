6ef063327b4aad6143cf5b3773746aa7003e391f
AndersenCastaneda <andersenpiano@gmail.com>
2020-12-20 15:36:54 -0500

    Add task 3

---
+++ 0x08-csharp-inheritance/3-type_get/3-type_get.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+
+/// <summary>
+/// Class Obj
+/// </summary>
+public class Obj
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <param name="myObj"></param>
+    public static void Print(object myObj)
+    {
+        var t = myObj.GetType().GetTypeInfo();
+        Console.WriteLine($"{t.Name} Properties:");
+
+        foreach (PropertyInfo p in t.GetProperties())
+            Console.WriteLine(p.Name);
+
+        Console.WriteLine($"{t.Name} Methods:");
+        foreach (MethodInfo m in t.GetMethods())
+            Console.WriteLine(m.Name);
+    }
+}
